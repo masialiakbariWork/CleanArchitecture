914516bfa73c9ab0daa43092afb12161b015d079
Aliakbari <MasiAliakbari69@gmail.com>
2025-02-22 10:11:25 +0330

    sc

--- Presentation/Program.cs
+++ Presentation/Program.cs
@@ -5,29 +5,22 @@ using Infrastructure;
 using Domain.Interfaces;
 using Infrastructure.Persistence;
 using Microsoft.EntityFrameworkCore;
-using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services
-    .AddInfrastructure()
     .AddApplication()
+    .AddInfrastructure()
     .AddPresentation()
     .AddDomain();
 
 // تنظیمات مربوط به EF Core و دیتابیس
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("CleanArchDB")));
 
 // تنظیمات Dependency Injection برای UserRepository
 builder.Services.AddScoped<IUserRepository, UserRepository>();
-
-// افزودن MediatR برای استفاده از CQRS
-builder.Services.AddMediatR(cfg =>
-{
-    cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
-});
 
 builder.Services.AddControllers();
 
